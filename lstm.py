998e8e120c725847edae9532b08fca79c75425a1
Ayelet Pass <73468339+ayeletpass@users.noreply.github.com>
2021-04-06 18:56:21 +0300

    split code to functions

--- lstm.py
+++ lstm.py
@@ -1,12 +1,4 @@
-
-from pandas import read_csv
 from datetime import datetime
-# load data
-#def parse(x):
-#	return datetime.strptime(x, '%Y %m %d %H')
-
-dataset = read_csv('simulator.ver1.csv', index_col=-1)
-
 from math import sqrt
 from numpy import concatenate
 from matplotlib import pyplot
@@ -20,77 +12,90 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 
-# load dataset
-dataset = read_csv('simulator.ver1.csv', header=0)
-values = dataset.values
-# ensure all data is float
-values = values.astype('float32')
-# normalize features
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled = scaler.fit_transform(values)
+def load_dataset(path):
+    # load dataset
+    dataset = read_csv(path, header=0)
+    values = dataset.values
+    # ensure all data is float
+    values = values.astype('float32')
+    # normalize features
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+    return scaled
 
-# split into train and test sets
-n = scaled.shape[0]
-print(n, type(n), scaled.shape)
-train = scaled[:int(n*0.9), :]
-test = scaled[int(n*0.9):, :]
-# split into input and outputs
-train_X, train_y = train[:, :-2], train[:, -2:]
-test_X, test_y = test[:, :-2], test[:, -2:]
-# reshape input to be 3D [samples, timesteps, features]
-train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+def split_train_test(scaled):
+    # split into train and test sets
+    n = scaled.shape[0]
+    train = scaled[:591, :]
+    test = scaled[591:1682, :]
+    # split into input and outputs
+    train_X, train_y = train[:, :-2], train[:, -2:]
+    test_X, test_y = test[:, :-2], test[:, -2:]
+    # reshape input to be 3D [samples, timesteps, features]
+    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    return train_X, train_y, test_X, test_y
 
-model = Sequential()
-model.add(LSTM(train_X.shape[2], input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(2))
-model.compile(loss='mae', optimizer='adam')
+def lstm(train_X, train_y, test_X, test_y, params):
+    epochs, batch_size = params
+    model = Sequential()
+    model.add(LSTM(train_X.shape[2], input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(2))
+    model.compile(loss='mae', optimizer='adam')
+    # fit network
+    history = model.fit(train_X, train_y, epochs=epochs, batch_size=batch_size, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+    # make a prediction
+    yhat = model.predict(test_X)
+    return history, yhat
 
-# fit network
-history = model.fit(train_X, train_y, epochs=300, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+def calculate_rmse(y_predict, test):
+    test_X, test_y = test
+    test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
-# plot history
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
-pyplot.legend()
-pyplot.show()
+    # invert scaling for forecast
+    inv_yhat = concatenate((yhat, test_X[:, :]), axis=1)
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:,0]
+    # invert scaling for actual
+    test_y = test_y.reshape((len(test_y), 2))
+    inv_y = concatenate((test_y, test_X[:, :]), axis=1)
 
-# make a prediction
-yhat = model.predict(test_X)
+    inv_y = scaler.inverse_transform(inv_y)
+    inv_y = inv_y[:,0]
+    # calculate RMSE
+    rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+    return round(rmse, 3)
 
-# Graph 1
-pyplot.title("Prediction y1")
-pyplot.plot(yhat[:, 0], 'red', label='y1 predict rain')
-pyplot.plot(test_y[:, 0], 'blue', label='y1 test rain')
-pyplot.legend()
-pyplot.show()
-# Graph 2
-pyplot.title("Prediction y2")
-pyplot.plot(yhat[:, 1], 'red', label='y2 predict cloud')
-pyplot.plot(test_y[:, 1], 'blue', label='y2 test cloud')
-pyplot.legend()
-pyplot.show()
+def display_plot(title, x, y):
+    # plot history
+    pyplot.title(title)
+    pyplot.plot(x[0], 'red', label=x[1])
+    pyplot.plot(y[0], 'blue', label=y[1])
+    pyplot.legend()
+    pyplot.show()
 
-test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
-#train_X = train_X.reshape((train_X.shape[0], train_X.shape[2]))
+def main(path, params):
+    scaled = load_dataset(path)
+    train_X, train_y, test_X, test_y = split_train_test(scaled)
+    history, y_predict = lstm(train_X, train_y, test_X, test_y, params)
+    rmse = calculate_rmse(y_predict, [test_X, test_y])
+    print('rmse', rmse)
+    # plot history
+    display_plot("history", 
+                 [history.history['loss'], 'train'],
+                 [history.history['val_loss'], 'test'])
+    # Graph 1
+    display_plot("Prediction y1",
+                 [y_predict[:, 0], 'y1 predict rain'],
+                [test_y[:, 0], 'y1 test rain'])
+    # Graph 2    
+    display_plot("Prediction y2",
+                 [y_predict[:, 1], 'y2 predict rain'],
+                 [test_y[:, 1], 'y2 test rain']) 
+	return rmse
 
-# invert scaling for forecast
-inv_yhat = concatenate((yhat, test_X[:, :]), axis=1)
-#print(inv_yhat.shape)
-#print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
-
-#inv_yhat = inv_yhat.reshape((inv_yhat.shape[0], inv_yhat.shape[2]))
-#print(inv_yhat.shape)
-inv_yhat = scaler.inverse_transform(inv_yhat)
-inv_yhat = inv_yhat[:,0]
-# invert scaling for actual
-test_y = test_y.reshape((len(test_y), 2))
-inv_y = concatenate((test_y, test_X[:, :]), axis=1)
+if __name__ == "__main__":
+    main("simulator.ver1.csv", [450, 42])
 
 
-inv_y = scaler.inverse_transform(inv_y)
-inv_y = inv_y[:,0]
-# calculate RMSE
-rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-print('***Test RMSE: %.3f***' % rmse)
 
